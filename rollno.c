be753bc8cf9df9bff9562b2613772f3cbd581052
ann-mariyatony <annmariyatony2028@cs.sjcetpalai.ac.in>
2025-03-25 16:21:36 +0530

    Add files via upload

---
+++ rollno.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int rollno,age;
+printf("Enter the roll no:");
+scanf("%d",&rollno);
+printf("\n Enter age:");
+scanf("%d",&age);
+printf("\n The roll no is:%d",rollno);
+printf("\nThe age is %d",age);
+return 0;
+}
